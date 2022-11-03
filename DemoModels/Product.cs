9821abdfba7e9d5ed638320490bf928ddfb263b3
Simon Attila <simon.attila1979@gmail.com>
2022-11-03 12:06:37 +0100

    demo modell nullable fields

--- DemoModels/Product.cs
+++ DemoModels/Product.cs
@@ -13,11 +13,11 @@ namespace DemoModels
         public Guid Id { get; set; }
         public string? Code { get; set; }
         public string? Name { get; set; }
-        public bool Active { get; set; }
-        public double Price { get; set; }
+        public bool? Active { get; set; }
+        public double? Price { get; set; }
         public DateTime? ReleaseDate { get; set; }
-        public int Rating { get; set; }
-        public ProductType Type {  get; set; }
+        public int? Rating { get; set; }
+        public ProductType? Type {  get; set; }
         public ProductExt? Ext { get; set; }
         public List<InventoryStock>? Stocks { get; set; }
 
