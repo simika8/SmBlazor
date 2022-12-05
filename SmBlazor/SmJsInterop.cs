9db606e5857c0caaeeebb740d7c8e8df503fdf5e
Simon Attila <simon.attila1979@gmail.com>
2022-12-05 16:10:35 +0100

    jsinterop refactor

--- SmBlazor/SmJsInterop.cs
+++ SmBlazor/SmJsInterop.cs
@@ -12,98 +12,32 @@ namespace SmBlazor
 
     public class SmJsInterop : IAsyncDisposable
     {
-        private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+        private readonly Lazy<Task<IJSObjectReference>> _moduleTask;
 
         public SmJsInterop(IJSRuntime jsRuntime)
         {
-            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+            _moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
                "import", "./_content/SmBlazor/smJsInterop.js").AsTask());
         }
-        public async ValueTask<bool> ScrollToElement(ElementReference? element)
+        
+        public async Task<IJSObjectReference> GetJsObjectReference()
         {
-            if (element == null)
-                return false;
-            var module = await moduleTask.Value;
-            try
-            {
-                return await module.InvokeAsync<bool>("scrollToElement", element);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                return false;
-            }
-        }
-        public async ValueTask<int> ClientHeight(ElementReference? element)
-        {
-            if (element == null)
-                return 0;
-            var module = await moduleTask.Value;
-            try
-            {
-                var res = await module.InvokeAsync<int>("clientHeight", element);
-                return res;
-            }
-            catch
-            {
-                return 0;
-            }
-        }
-        public async ValueTask<int> GetScrollTop(ElementReference? element)
-        {
-            if (element == null)
-                return 0;
-            var module = await moduleTask.Value;
-            try
-            {
-                var res = await module.InvokeAsync<int>("GetScrollTop", element);
-                return res;
-            }
-            catch
-            {
-                return 0;
-            }
+            return await _moduleTask.Value;
         }
 
-        public async ValueTask<bool> FocusToElement(ElementReference? element)
-        {
-            if (element == null)
-                return false;
-            var module = await moduleTask.Value;
-            return await module.InvokeAsync<bool>("focusToElement", element);
-        }
-        public async ValueTask<bool> SetValue(ElementReference? element, string text)
-        {
-            if (element == null)
-                return false;
-            var module = await moduleTask.Value;
-            return await module.InvokeAsync<bool>("setValue", element, text);
-        }
-
-        public async ValueTask<bool> SubscribeToChange(DotNetObjectReference<Input> inputComponentRef, ElementReference? InputElementRef)
-        {
-            if (InputElementRef == null)
-                return false;
-            var module = await moduleTask.Value;
-            await module.InvokeVoidAsync("subscribeToChange", new object[] { inputComponentRef, InputElementRef });
-            return true;
-        }
-
-        public async ValueTask<bool> AddEventListenerScroll(DotNetObjectReference<Grid> dotnetComponentRef, ElementReference? htmlElementRef)
-        {
-            if (htmlElementRef == null)
-                return false;
-            var module = await moduleTask.Value;
-            await module.InvokeVoidAsync("addEventListenerScroll", new object[] { dotnetComponentRef, htmlElementRef });
-            return true;
-        }
 
         public async ValueTask DisposeAsync()
         {
-            if (moduleTask.IsValueCreated)
+            try
             {
-                var module = await moduleTask.Value;
-                await module.DisposeAsync();
+                if (_moduleTask.IsValueCreated)
+                {
+                    var module = await _moduleTask.Value;
+                    await module.DisposeAsync();
+                }
+            } catch 
+            {
+
             }
         }
     }
