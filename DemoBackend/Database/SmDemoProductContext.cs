5267d0626b907008ddcca74d62db347a6a92d99b
Simon Attila <simon.attila1979@gmail.com>
2022-12-21 10:06:27 +0100

    removetables

--- DemoBackend/Database/SmDemoProductContext.cs
+++ DemoBackend/Database/SmDemoProductContext.cs
@@ -12,26 +12,48 @@ public class SmDemoProductContext : DbContext
 
     public DbSet<DemoModels.Product> Product { get; set; } = null!;
 
+    private static void RemoveTables()
+    {
+        using var db = new SmDemoProductContext();
+
+        var tableNames = db.Database.SqlQueryRaw<string>("SELECT table_name FROM information_schema.tables where information_schema.tables.table_schema = 'public'").ToList();
+        foreach (var tableName in tableNames)
+        {
+            db.Database.ExecuteSqlRaw($"drop table if exists \"{tableName}\" cascade;");
+        }
+        ;
+    }
     public static void InitRandomData()
     {
         if (Initialized)
             return;
         Initialized = true;
         using var db = new SmDemoProductContext();
-        var prodCount = 100000;
-        db.Database.Migrate();
-        var oldcount = db.Product.Count();
-        if (oldcount == prodCount)
-            return;
+        var prodCount = 10000;
+        try
+        {
+            db.Database.Migrate();
+            var oldcount = db.Product.Count();
+            if (oldcount == prodCount)
+                return;
 
-        while (db.Product.Count() > 0)
+        } catch (Exception ex)
+        {
+            RemoveTables();
+            db.Database.Migrate();
+        }
+
+        RemoveTables();
+        db.Database.Migrate();
+
+        /*while (db.Product.Count() > 0)
         {
             using var db3 = new SmDemoProductContext();
             var toremove = db3.Product.Take(1000);
             db3.Product.RemoveRange(toremove);
             db3.SaveChanges();
-        }
-        
+        }*/
+
 
 
         var inmem = DictionaryDatabase.GetProductDict(prodCount).Select(x => x.Value).ToList();
