9839a79293b32ac52124316a213b8a73762e0085
Simon Attila <simon.attila1979@gmail.com>
2026-01-06 16:34:12 +0000

    Scrollbar width

--- SmBlazor/Settings/StyleSettings.cs
+++ SmBlazor/Settings/StyleSettings.cs
@@ -9,46 +9,37 @@ namespace SmBlazor
     [Flags] public enum StyleSettings
     {
         None = 0,
-        GridHeight = 1,
-        ColumnWidth = 2,
-        ColumnAlign = 4,
-        Basic = 8,
-        Extra = 16,
-        BasicScrollBar = 32,
-        ExtraScrollBar = 64,
+        SetGridHeight = 1,
+        SetColumnWidth = 2,
+        Extra = 8,
+        ScrollBar8 = 16
     }
     public static class StyleSettingsHelper
     {
         public static StyleSettings GetDefaultStyleSettings()
         {
-            return 
-                StyleSettings.GridHeight
-            | StyleSettings.ColumnWidth
-            | StyleSettings.ColumnAlign
-            | StyleSettings.Basic
+            return
+            StyleSettings.None
             | StyleSettings.Extra
-            | StyleSettings.ExtraScrollBar
+            | StyleSettings.SetGridHeight
+            | StyleSettings.SetColumnWidth
+            //| StyleSettings.ScrollBar8
             ;
         }
 
         public static string GetStyleClasses(this StyleSettings styleSettings)
         {
             var styleClassList = new List<string>();
-            if (styleSettings.HasFlag(StyleSettings.Basic))
-            {
-                styleClassList.Add("BS");
-            }
             if (styleSettings.HasFlag(StyleSettings.Extra))
             {
                 styleClassList.Add("ES");
             }
-            if (styleSettings.HasFlag(StyleSettings.BasicScrollBar))
+            if (styleSettings.HasFlag(StyleSettings.ScrollBar8))
             {
-                styleClassList.Add("BSB");
-            }
-            if (styleSettings.HasFlag(StyleSettings.ExtraScrollBar))
+                styleClassList.Add("SB8"); //8 px width scrollbar
+            } else
             {
-                styleClassList.Add("ESB");
+                styleClassList.Add("SBT"); //thin scrollbar
             }
             var res = string.Join(" ", styleClassList);
             return res;
